c67fdd25229deada7cd943b3e123179880e7ab4f
theeorooda-star <theeorooda@gmail.com>
2026-03-05 12:50:33 -0300

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,1 +1,8 @@
+edad = 18
 
+if edad >= 18:
+    print("Podés pasar, sos mayor de edad.")
+elif edad >= 16:
+    print("Podés pasar solo con autorización de tus padres.")
+else:
+    print("No podés pasar, sos muy chico.")
